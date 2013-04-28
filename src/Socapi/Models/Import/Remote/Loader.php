bcf83ed8174fb8161ea737191d53c32ff3c3aa88
Niktux <nico.ln@gmail.com>
2013-04-28 15:04:14 +0200

    Init loaders

--- src/Socapi/Models/Import/Remote/Loader.php
+++ src/Socapi/Models/Import/Remote/Loader.php
@@ -2,39 +2,8 @@
 
 namespace Socapi\Models\Import\Remote;
 
-class Loader
+interface Loader
 {
-    const
-        ROOT_URL_PATTERN = 'http://www.lfp.fr/ligue1/competitionPluginCalendrierResultat/changeCalendrierJournee?sai=%d&jour=%d';
-    
-    private
-        $season;
-    
-    public function __construct($season)
-    {
-        $this->season = $season;
-    }
-    
-    public function load($day)
-    {
-        $url = $this->computeUrl($day);
-        $html = file_get_contents($url);
-        
-        // file_put_contents("cache/html/$day.html", $html);
-        
-        return $html;
-    }
-    
-    private function computeUrl($day)
-    {
-        return sprintf(self::ROOT_URL_PATTERN, $this->season, $day);
-    }
-    
-    public function loadMatch($matchId)
-    {
-        $url = sprintf('http://www.lfp.fr/ligue1/feuille_match/%d', $matchId);
-        $html = file_get_contents($url);
-        
-        return $html;
-    }
+    public function load($day);
+    public function loadMatch($matchId);
 }
